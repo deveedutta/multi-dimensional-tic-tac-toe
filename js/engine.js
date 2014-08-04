cb0708af2ad24e364acd48e1c1ae4ca63c26de59
Deveedutta Maharana <deveedutta@gmail.com>
2014-08-04 15:14:22 +0530

    Post Lunch Game engine Logic alterations
    Trying to figureout what’s wrong with the logic I planned

--- js/engine.js
+++ js/engine.js
@@ -5,14 +5,68 @@ $( function () {
     
         _el              = null                                             // Private reference to the jQuery element on which we draw
     ,   _game            = []                                               // Array to maintain 'LIFE'
+    ,   _player1         = []                                               // Not sure if I need this, but Player one needs some space I thik
+    ,   _player2         = []                                               // So does player 2
     ,   _cell            = {}                                               // Object that keeps a record of the corresponding <td> tags
     ,   _options         = {}                                               // Private copy of the options
     ,   _count           = 0                                                // Running time
+    ,   _score1          = 0                                                // Player 1's scrore
+    ,   _score2          = 0                                                // Player 2's scrore
+    ,   _playerTurn      = false                                            // Altering turns for Player 1 & 2 with a true:false flag
     ;
     
     var engine = {
         init : init
     }
+    
+    
+    function scanForWinner() {
+        var i, j;
+        
+        
+        for ( i=0,j=0; i < _options.size ; i++, j++ ){                      // Scan diagonaly
+                evaluate (_game[i][j]);
+        }
+        
+        for ( i=0; i < _options.size ; i++){
+            for ( j=0; j< _options.size ; j++ ) {                           // Scan row-wise
+                evaluate (_game[i][j]);
+            }
+        }
+        
+        for ( j=0; j < _options.size ; j++){
+            for ( i=0; i< _options.size ; i++ ) {                           // Scan column-wise
+                evaluate (_game[i][j]);
+            }
+        }
+        console.log(_score1, _score2);
+        
+    }
+    
+    function evaluate ( g ) {
+        if ( g == -1) {
+            _score1 = 0;
+            _score2 = 0;
+            return;
+        }
+        if ( g == 1) {
+            _score1++;
+        }
+        if ( g == 2) {
+            _score2++;
+        }
+        if ( _score1 === _options.size ) {
+            alert ("Player 1 is the winner");
+            //resetGame ();
+            return;
+        }
+        if ( _score2 === 2 * _options.size ) {
+            alert ("Player 2 is the winner");
+            //resetGame ();
+            return;
+        }
+    }
+    
     
     
     function init  ( el, options ) {
@@ -29,6 +83,8 @@ $( function () {
         
         drawBoard ();
     }
+    
+
     
     function drawBoard() {
         var 
@@ -49,19 +105,32 @@ $( function () {
                 
                 $(tr).append( td );
 
-                _game [ i ][ j ] = 0;
+                _game [ i ][ j ] = -1;
                 _cell [ 'td-' + i +',' + j ] = td;
                 
                 
                 $(td).on ('click', function () {
                     $td = $(this);
-                    $td.attr('class', 'active');
+                    
 
                     var id = $td.attr('id').split('td-')[1];
                     var X = id.split(',')[0] * 1;
                     var Y = id.split(',')[1] * 1;
                     
-                    _game [ X ][ Y ] = 1;
+                   
+                    
+                    if ( _playerTurn ){
+                        $td.attr('class', 'player1');
+                        _game [ X ][ Y ] = 1;
+                        _playerTurn = false;
+                    } else {
+                        $td.attr('class', 'player2');
+                        _game [ X ][ Y ] = 2;
+                        _playerTurn = true;
+                    }
+                    
+                    scanForWinner();
+                    $td.off();
                 });
                 
             }
