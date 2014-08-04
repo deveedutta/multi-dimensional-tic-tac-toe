795c1cee2e8462bc34d237c6860575bd24e99401
Deveedutta Maharana <deveedutta@gmail.com>
2014-08-04 10:30:11 +0530

    Another
    before power cut

--- js/engine.js
+++ js/engine.js
@@ -3,10 +3,10 @@ window.game = {};
 $( function () {
         var
     
-        _el              = null                                             // Private reference to the html element on which we draw
+        _el              = null                                             // Private reference to the jQuery element on which we draw
     ,   _game            = []                                               // Array to maintain 'LIFE'
     ,   _cell            = {}                                               // Object that keeps a record of the corresponding <td> tags
-    ,   _options         = { }                                              // Private copy of the options
+    ,   _options         = {}                                               // Private copy of the options
     ,   _count           = 0                                                // Running time
     ;
     
@@ -21,11 +21,57 @@ $( function () {
         else if ( el instanceof HTMLElement || el instanceof String )
             _el = $(el);
         
+        if( !_el ) return;
             
-        if ( ! _el instanceof HTMLElement ) return;
-            
+        for ( item in options ) {
+            _options [ item ] = options [ item ];
+        }
         
+        drawBoard ();
     }
     
-    window.game = {};
+    function drawBoard() {
+        var 
+            i,j
+            table       = $('<table>')
+        ;
+        
+        for ( i = 0; i < _options.size ; i++ ) {
+            
+            var tr = $( '<tr>' )
+            
+            _game [ i ] = new Array( _options.size );
+            
+            for ( j = 0; j < _options.size ; j++ ) {
+
+                var td = $( '<td>' );
+                $(td).attr( 'id', 'td-' + i + ',' + j );
+                
+                $(tr).append( td );
+
+                _game [ i ][ j ] = 0;
+                _cell [ 'td-' + i +',' + j ] = td;
+                
+                
+                $(td).on ('click', function () {
+                    $td = $(this);
+                    $td.attr('class', 'active');
+
+                    var id = $td.attr('id').split('td-')[1];
+                    var X = id.split(',')[0] * 1;
+                    var Y = id.split(',')[1] * 1;
+                    
+                    _game [ X ][ Y ] = 1;
+                });
+                
+            }
+            
+            $(table).append ( tr );
+        }
+        
+        _el.html("");
+        _el.append(table);
+    }
+    
+    window.game = engine;
 } );
